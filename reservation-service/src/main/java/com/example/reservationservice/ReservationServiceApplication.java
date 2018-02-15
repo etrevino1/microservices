7a772b5ecbfe9e9d842d9cc346da89c79093c64e
etrevino1 <etrevino1@hotmail.com>
2018-02-15 16:18:32 -0600

    1

--- reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -8,15 +8,19 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ReservationServiceApplication {
@@ -41,6 +45,18 @@ class DummyDataCLR implements CommandLineRunner {
 	@Override
 	public void run(String...strings) throws Exception {
 
+	}
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+	@Value("${message}")
+	private String msg;
+	
+	@RequestMapping("/message")
+	String message() {
+		return this.msg;
 	}
 }
 
